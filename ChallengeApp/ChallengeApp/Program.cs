29e484daccd44417a72b786567451595bee5e8a1
Krzysztof Gajda <gajdamail@gmail.com>
2023-10-10 11:07:45 +0200

    Zadanie domowe - dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Marta";
+char sex = 'k';
+var age = 17;
+
+if (sex == 'm' || sex == 'M')
+{
+    if (age < 18)
+    {
+        Console.WriteLine($"Niepełnoletni mężczyzna o imieniu {name}.");
+    }
+    else
+    {
+        Console.WriteLine($"{name}, mężczyzna lat {age}.");
+    }
+}
+else if (sex == 'k' || sex == 'K')
+{
+    if (age < 18)
+    {
+        Console.WriteLine($"Niepełnoletnia kobieta o imieniu {name}.");
+    }
+    else
+    {
+        Console.WriteLine($"{name}, kobieta lat {age}.");
+    }
+}
