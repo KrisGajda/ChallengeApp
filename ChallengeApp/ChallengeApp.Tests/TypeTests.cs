3756deb6bffb3bca4469ecd5989246075c54edcf
Krzysztof Gajda <gajdamail@gmail.com>
2023-10-22 14:27:21 +0200

    Zadanie dzień 8. Testy typów referencyjnych i wartościowych.

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,49 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TestShouldReturntTrueIfStringTypeIsAValueType()
+        {
+            // arrange
+            string name1 = "Adam";
+            string name2 = "Adam";
+            // assert
+            Assert.AreEqual(name1, name2);
+        }
+
+        [Test]
+        public void TestShouldReturntTrueIfIntegerTypeIsAValueType()
+        {
+            // arrange
+            int age1 = 27;
+            int age2 = 27;
+            // assert
+            Assert.AreEqual(age1, age2);
+        }
+
+        [Test]
+        public void TestShouldReturntTrueIfFloatTypeIsAValueType()
+        {
+            // arrange
+            float distanceInKilometers1 = 140.24f;
+            float distanceInKilometers2 = 140.24f;
+            // assert
+            Assert.AreEqual(distanceInKilometers1, distanceInKilometers2);
+        }
+
+        [Test]
+        public void GetEmployeeShouldReturnDifferentObjects()
+        {
+            // arrange
+            var user1 = GetEmployee("Adam", "Adamowski", 36);
+            var user2 = GetEmployee("Adam", "Adamowski", 36);
+            // assert
+            Assert.AreNotEqual(user1, user2);
+        }
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}
